def3da55d21f819e8954244bb9b6ce7b692be44e
fiba-dev <fibarrola06@gmail.com>
2022-03-23 16:32:00 -0400

    change auth

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -2,6 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { sendCode } from "controller/auth";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-	const results = await sendCode(req.body.email);
-	res.send(results);
+	try {
+		const results = await sendCode(req.body.email);
+		res.send(results);
+	} catch (error) {
+		res.status(404).send(error);
+	}
 }
