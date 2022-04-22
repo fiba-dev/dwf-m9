df51e8f1b3eada5941946427245985268dc0a49e
fiba-dev <fibarrola06@gmail.com>
2022-04-22 14:47:32 -0400

    edit cors 2

--- lib/init-middleware.ts
+++ lib/init-middleware.ts
@@ -2,6 +2,8 @@ export default function initMiddleware(middleware) {
 	return (req, res) =>
 		new Promise((resolve, reject) => {
 			middleware(req, res, (result) => {
+				res.setHeader("Access-Control-Allow-Credentials", true);
+				res.setHeader("Access-Control-Allow-Origin", "*");
 				if (result instanceof Error) {
 					return reject(result);
 				}
