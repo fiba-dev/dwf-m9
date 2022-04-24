fe347bccd41d10b81167079ce47739a23558ec92
fiba-dev <fibarrola06@gmail.com>
2022-04-24 13:00:01 -0400

    asdlk

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -36,5 +36,5 @@ const handlerAuth = method({
 });
 export default async function handler(req, res) {
 	await cors(req, res);
-	authMiddleware(handlerAuth);
+	res.send(authMiddleware(handlerAuth));
 }
