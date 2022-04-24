23581e21d60cad6e03d63a998ffb5f78995c99b2
fiba-dev <fibarrola06@gmail.com>
2022-04-24 18:54:09 -0400

    edit cors2

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -31,6 +31,6 @@ export default async function handler(req, res) {
 	// Run cors
 	await cors(req, res);
 	// Rest of the API logic
-	res.json({ message: "Hello Everyone!" });
+	return authMiddleware(handler);
 }
 // export default middlewareCors(authMiddleware(handlerAuth));
