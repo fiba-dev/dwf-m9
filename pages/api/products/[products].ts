6b03b6a2afe5f2540f0cab90d658d96f320b37f7
fiba-dev <fibarrola06@gmail.com>
2022-04-25 10:33:19 -0400

    edit get products

--- pages/api/products/[products].ts
+++ pages/api/products/[products].ts
@@ -2,8 +2,32 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 import { authMiddleware } from "lib/middlewares";
 import { getProduct } from "controller/products";
+import Cors from "cors";
+const cors = Cors({
+	methods: ["GET", "POST", "HEAD"],
+});
 
-export default async function getProducts(
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
+function authMiddlewareCors(callback) {
+	const data = {};
+	return async function (req: NextApiRequest, res: NextApiResponse) {
+		const respuesta = await runMiddleware(req, res, cors);
+		callback(req, res, data);
+	};
+}
+export default authMiddlewareCors(async function getProducts(
 	req: NextApiRequest,
 	res: NextApiResponse,
 	userBody
@@ -13,7 +37,4 @@ export default async function getProducts(
 	if (resultado == false)
 		return res.status(404).send({ message: "Product not found" });
 	res.send(resultado);
-}
-const handler = methods({
-	get: getProducts,
 });
